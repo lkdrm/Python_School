262047538e99d386dcb5b33f81b2099889eee564
Vitalik <75811667+lkdrm@users.noreply.github.com>
2021-12-08 21:00:35 +0100

    Add files via upload

---
+++ 08.12/08.12.py
@@ -0,0 +1,18 @@
+#print("Nothing\nwill work\nunless you do")
+#print("\t\tNothing", "will work", unless you do", sep="\n\t\t\t")
+#print()
+#print("'Anyone who'",
+# "stops", 
+# "\tlearning is old",
+# "\t\twheither at twenty or eighty.",
+# "\t\t\tHenry Ford" 
+# sep="\n\t")
+
+a=int(input(("Enter first numb:")))
+b=int(input(("Enter second numb:")))
+
+print("The first numb is:",(a), "\nThe second numb is:",(b), "\nThe sum is:", a+b)
+
+x = int(input("The first numb is:"))
+y = int(input("The second numb is:"))
+print("Number is", int((x/100)*y))
