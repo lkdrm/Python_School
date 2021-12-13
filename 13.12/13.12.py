cd005723e33f43d198d7530ff341c7c10f243089
Vitalik <75811667+lkdrm@users.noreply.github.com>
2021-12-13 21:00:54 +0100

    Add files via upload

---
+++ 13.12/13.12.py
@@ -0,0 +1,86 @@
+"""
+numb = 8 
+
+numb2 = 6 - 5
+
+numb3 = numb - numb2
+                    
+numb3 = numb3 + 2
+
+numb4 = "Hello"
+print(numb2)
+"""
+
+#numb_1=int(input("Enter the first numb:"))
+#numb_2=int(input("Enter the second numb:"))
+
+#print("The sum is:", numb_1 + numb_2)
+
+#2
+
+#user_numb = int(input("Enter three numbs:"))
+
+#print("The last is:",user_numb%10,
+#        "The mid is:",(user_numb//10%10),
+#        "The first is:", (user_numb//100),
+#        "The sum of numb is:", user_numb%10 + user_numb//10%10 + user_numb//100, sep="\n\t\t")
+
+
+#3
+"""
+sirka = int(input("Enter sirku obdelnik:"))
+vyska = int(input("Enter vysku obdelnika:"))
+
+obsah = sirka * vyska
+
+print("Obsah obdelnika je:", obsah)
+"""
+
+
+######
+"""
+numb_1=1
+numb_2=2
+
+print(numb_1<numb_2)
+print(numb_1 !=numb_2)
+
+"""
+
+########
+"""
+weather = input("How today is:").lower()
+
+if weather == "good":
+    print("Go out")
+    print("Turn on a music and chill")
+elif weather == "bad":
+    print("just learn python")
+else:
+    print("You just write incorrect")
+
+print("End of program")
+"""
+
+####
+"""
+a = int(input("Enter the first numb:"))
+b = int(input("Enter the second numb:"))
+
+if a > b :
+    print("The", a , "is bigger")
+elif a < b :
+    print("The", b , "is bigger")
+else:
+    print("They are same")
+
+"""
+####
+
+a = int(input("Enter the numb:"))
+
+if a%2 ==0:
+    print("The numb is even")
+elif a%3 ==0:
+    print("The numb is odd")
+
