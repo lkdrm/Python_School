0a26bf89eac20d4b4537626cc71c2b0e41e32aab
Vitalik <75811667+lkdrm@users.noreply.github.com>
2022-01-12 19:59:56 +0100

    Update prvo.py

--- 01.10/prvo.py
+++ 01.10/prvo.py
@@ -1,15 +1,16 @@
-l = [1,2,3,4,5,6,7,8,9,10,11,12,13]   
-new_prime = []
-#Zjistuje jestli je prvocislo
-def prime(a):
-    for b in a:
-        for i in range(2,b+1):
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13]   
+
+def prime(b):
+        for i in range(2,b):
             if b%i == 0:
                return False
         return True
 
-def prime_list(n_l):
-    n_l = prime(a)
+def prime_list(ln):
+    new_prime = []
+    for number in ln:
+        if prime(number):
+            new_prime.append(number)
+    return new_prime
 
-prime_list()
-#print(new_prime)
+print(prime_list(a))
