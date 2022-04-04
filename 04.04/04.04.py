26885705e7a905c1af06155beb9104d0eb8f6bd7
Vitalik <75811667+lkdrm@users.noreply.github.com>
2022-04-04 21:00:05 +0200

    Add files via upload

---
+++ 04.04/04.04.py
@@ -0,0 +1,20 @@
+# pip install Django
+# django-admin startproject animals 
+# or
+# python -m django startproject (name)
+
+# py manage.py
+
+# py manage.py runserver
+
+"""
+architektura MVC = model view controler
+
+Django ma ale MVT(nekdy i MTV)
+
+model je stale model, template je misto view
+a view jr misto controlleru
+
+smerova, router
+
+"""
